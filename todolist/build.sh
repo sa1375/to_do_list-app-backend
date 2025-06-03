bea4396814c7a0a1764c2e1906dc337042bf8294
sa1375 <140366511+sa1375@users.noreply.github.com>
2025-06-03 06:46:01 +0330

    Update build.sh

--- todolist/build.sh
+++ todolist/build.sh
@@ -1,9 +1,1 @@
-#!/usr/bin/env bash
-# Exit on any error
-set -o errexit
 
-echo "Running migrations..."
-python manage.py migrate --noinput
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
